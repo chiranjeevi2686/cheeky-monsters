6bb80eadee7b60e4e6606e7303a5459469415495
Chiru <kummu.infosys@gmail.com>
2020-06-12 17:12:00 +1000

    Updates

---
+++ precache-manifest.0f4732d8abf2cd2b35a58382d7e53f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c604df1fa95d38a6abfb20b75a160378",
+    "url": "/cheeky-monsters/index.html"
+  },
+  {
+    "revision": "5bd11d89030daa0fcb2c",
+    "url": "/cheeky-monsters/static/css/main.b91277fc.chunk.css"
+  },
+  {
+    "revision": "bc1bab706fd378f60c77",
+    "url": "/cheeky-monsters/static/js/2.eb46bd24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cheeky-monsters/static/js/2.eb46bd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd11d89030daa0fcb2c",
+    "url": "/cheeky-monsters/static/js/main.706c4511.chunk.js"
+  },
+  {
+    "revision": "b8fb43b198d0a0f6fa62",
+    "url": "/cheeky-monsters/static/js/runtime-main.d86ee901.js"
+  }
+]);
